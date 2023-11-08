a408efe83db100f2442cbbce9f2c9c0f6916159d
lupengfei3 <lupengfei3@guazi.com>
2023-11-08 14:08:41 +0800

    commit by lupengfei3

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.zsy.tool.zsy_tools {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.zsy.tool.zsy_tools to javafx.fxml;
-    exports com.zsy.tool.zsy_tools;
-}
